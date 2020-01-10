01dec464d969ccfadb963000f360439ff412ab5e
Cory Lynch <cjl2625@gmail.com>
2020-01-10 16:29:29 -0500

    Add base catbook

---
+++ client/src/components/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import NavBar from "./modules/NavBar.js";
+import { Router } from "@reach/router";
+import Feed from "./pages/Feed.js";
+import NotFound from "./pages/NotFound.js";
+import Profile from "./pages/Profile.js";
+import { get, post } from "../utilities.js";
+
+// to use styles, import the necessary CSS files
+import "../utilities.css";
+import "./App.css";
+
+/**
+ * Define the "App" component as a class.
+ */
+class App extends Component {
+  // makes props available in this component
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userId: null,
+    };
+  }
+
+  componentDidMount() {
+    get("/api/whoami").then((user) => {
+      if (user._id) {
+        // they are registed in the database, and currently logged in.
+        this.setState({ userId: user._id });
+      }
+    });
+  }
+
+  handleLogin = (res) => {
+    const userToken = res.tokenObj.id_token;
+    post("/api/login", { token: userToken }).then((user) => {
+      // the server knows we're logged in now
+      this.setState({ userId: user._id });
+      console.log(user);
+    });
+  };
+
+  handleLogout = () => {
+    console.log("Logged out successfully!");
+    this.setState({ userId: null });
+    post("/api/logout");
+  };
+
+  // required method: whatever is returned defines what
+  // shows up on screen
+  render() {
+    return (
+      // <> is like a <div>, but won't show
+      // up in the DOM tree
+      <>
+        <NavBar
+          handleLogin={this.handleLogin}
+          handleLogout={this.handleLogout}
+          userId={this.state.userId}
+        />
+        <div className="App-container">
+          <Router>
+            <Feed path="/" userId={this.state.userId} />
+            <Profile path="/profile/:userId" />
+            <NotFound default />
+          </Router>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ server/api.js
@@ -0,0 +1,78 @@
+/*
+|--------------------------------------------------------------------------
+| api.js -- server routes
+|--------------------------------------------------------------------------
+|
+| This file defines the routes for your server. 
+|
+*/
+
+const express = require("express");
+
+// import models so we can interact with the database
+const Story = require("./models/story");
+const Comment = require("./models/comment");
+const User = require("./models/user");
+const auth = require("./auth");
+
+// api endpoints: all these paths will be prefixed with "/api/"
+const router = express.Router();
+
+router.get("/stories", (req, res) => {
+  // empty selector means get all documents
+  Story.find({}).then((stories) => res.send(stories));
+});
+
+router.post("/story", (req, res) => {
+  const newStory = new Story({
+    creator_name: req.user.name,
+    creator_id: req.user._id,
+    content: req.body.content,
+  });
+
+  newStory.save().then((story) => res.send(story));
+});
+
+router.get("/comment", (req, res) => {
+  Comment.find({ parent: req.query.parent }).then((comments) => {
+    res.send(comments);
+  });
+});
+
+router.get("/user", (req, res) => {
+  User.findById(req.query.userid).then((user) => {
+    res.send(user);
+  });
+});
+
+router.post("/comment", (req, res) => {
+  const newComment = new Comment({
+    creator_name: req.user.name,
+    creator_id: req.user._id,
+    content: req.body.content,
+    parent: req.body.parent,
+    content: req.body.content,
+  });
+
+  newComment.save().then((comment) => res.send(comment));
+});
+
+router.post("/login", auth.login);
+router.post("/logout", auth.logout);
+
+router.get("/whoami", (req, res) => {
+  if (req.user) {
+    res.send(req.user);
+  } else {
+    // user is not logged in
+    res.send({});
+  }
+});
+
+// anything else falls to this "not found" case
+router.all("*", (req, res) => {
+  console.log(`API route not found: ${req.method} ${req.url}`);
+  res.status(404).send({ msg: "API route not found" });
+});
+
+module.exports = router;
